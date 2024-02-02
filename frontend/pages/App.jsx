c19debaa4da1d8b4852bd7f772eec81b292e2423
Jerzen Espiritu <jenjeeeeenespiritu@gmail.com>
2024-02-03 02:13:57 +0800

    updated

--- frontend/pages/App.jsx
+++ frontend/pages/App.jsx
@@ -5,16 +5,8 @@ import {
     Route 
 } 
     from 'react-router-dom';
-// import Dashboard from './Dashboard';
-// import Employee from './Employee';
-// import Home from './Home';
-// import TopbarEmployee from './TopbarEmployee';
-// import EmployeeDetails from './EmployeeDetails';
-// import EmployeeEdit from './EmployeeEdit';
-// import Form from './components/Form';
+
 import Login from './login';
-import Topbar from '../topbar';
-import Sidebar from '../sidebar';
 import Home from './home';
 import Register from './register';
 import Support from './support';
