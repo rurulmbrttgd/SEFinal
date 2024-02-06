7e407ff1baaa9678a25e2f73dbc83d7188846f8e
Jerzen Espiritu <jenjeeeeenespiritu@gmail.com>
2024-02-07 00:04:14 +0800

    dfg

--- frontend/components/backuprestore.jsx
+++ frontend/components/backuprestore.jsx
@@ -4,16 +4,25 @@ import './components.css'
 export default function Backup() {    
     return (
         <div className='col p-0 m-0 root-backup'>
+<<<<<<< Updated upstream
             <h1 className="title">Data Backup</h1>
             <h1 className="title2">Restore Data</h1>
             <div className="dataBackup">
                 <div className="mainBackup">
+=======
+                <div className="mainBackup">
+                    <h1 className="title">Data Backup</h1>
+>>>>>>> Stashed changes
                     <button className="DownloadData">Download Data</button>
                     <div className="backUp">
                         <p className="text">
                             From
                             <select className="dropdown">
+<<<<<<< Updated upstream
                                 <option>Seleect Month</option>
+=======
+                                <option>Select Month</option>
+>>>>>>> Stashed changes
                                 <option>January</option>
                                 <option>February</option>
                                 <option>March</option>
@@ -24,7 +33,11 @@ export default function Backup() {
                         <p className="text">
                             To
                             <select className="dropdown">
+<<<<<<< Updated upstream
                                 <option>Seleect Month</option>
+=======
+                                <option>Select Month</option>
+>>>>>>> Stashed changes
                                 <option>January</option>
                                 <option>February</option>
                                 <option>March</option>
@@ -36,10 +49,17 @@ export default function Backup() {
                 </div>
                 <div className="mainRestore">
                     <div className="Restore">
+<<<<<<< Updated upstream
                         <input type="file" className="fileUpload" />
                     </div>
                     <button className="import">Import</button>
                 </div>
+=======
+                    <h1 className="title2">Restore Data</h1>
+                        <input type="file" className="fileUpload" />
+                    </div>
+                    <button className="import">Import</button>
+>>>>>>> Stashed changes
             </div>
         </div>
     );
