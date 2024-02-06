0c819fadf19b8787f3c7f5ac5602949a53f902bf
Jerzen Espiritu <jenjeeeeenespiritu@gmail.com>
2024-02-07 00:32:56 +0800

    updated

--- frontend/components/backuprestore.jsx
+++ frontend/components/backuprestore.jsx
@@ -4,16 +4,14 @@ import './components.css'
 export default function Backup() {    
     return (
         <div className='col p-0 m-0 root-backup'>
-                <h1 className="title">Data Backup</h1>
-                <h1 className="title2">Restore Data</h1>
-                <div className="dataBackup">
                     <div className="mainBackup">
+                    <h1 className="title">Data Backup</h1>
                         <button className="DownloadData">Download Data</button>
                         <div className="backUp">
                             <p className="text">
                                 From
                                 <select className="dropdown">
-                                    <option>Seleect Month</option>
+                                    <option>Select Month</option>
                                     <option>January</option>
                                     <option>February</option>
                                     <option>March</option>
@@ -24,7 +22,7 @@ export default function Backup() {
                             <p className="text">
                                 To
                                 <select className="dropdown">
-                                    <option>Seleect Month</option>
+                                    <option>Select Month</option>
                                     <option>January</option>
                                     <option>February</option>
                                     <option>March</option>
@@ -36,11 +34,12 @@ export default function Backup() {
                     </div>
                     <div className="mainRestore">
                         <div className="Restore">
+                        <h1 className="title2">Restore Data</h1>
+
                             <input type="file" className="fileUpload" />
                         </div>
                         <button className="import">Import</button>
                     </div>
                 </div>
-        </div>
     );
 };
