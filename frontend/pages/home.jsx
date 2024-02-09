3da7a7dae4be3446c1d3aa46d1004076a3921c8f
Erica Cavaneyro <82349308+CavaneyroErica@users.noreply.github.com>
2024-02-09 17:45:17 +0800

    Update home.jsx, dashboard

--- frontend/pages/home.jsx
+++ frontend/pages/home.jsx
@@ -10,9 +10,19 @@ export default function Home() {
 return(
     <div className='root-home'>
         <Topbar/>
-        <div className='d-flex content-color'>
+        <div className='d-flex'>
             <Sidebar/>
+            <iframe 
+              className='centered-iframe'
+              width='600'
+              height='800'
+              src='https://lookerstudio.google.com/embed/reporting/8c1ff205-c43c-4041-a509-15e1fe12ecaa/page/OytpD'
+              frameBorder='0'
+              style={{ border: '0', width: '100%', height: '750px' }}
+              allowFullScreen
+            ></iframe>
         </div>
+      
     </div>
     );
 };
